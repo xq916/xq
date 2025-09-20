1214cdd696ca80db77192fe40b08fe3a87e9dda0
xq916 <2997738227@qq.com>
2025-09-20 22:46:06 +0800

    Add files via upload

---
+++ puzzle-server/puzzle-server/src/main/java/com/example/puzzle_server/PuzzleServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.puzzle_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleServerApplication.class, args);
+	}
+
+}
